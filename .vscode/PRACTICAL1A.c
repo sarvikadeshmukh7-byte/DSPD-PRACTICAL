9f251582672553a412e38f919e73617ff818bc9a
Sarvika Deshmukh <sarvikadeshmukh7@gmail.com>
2025-11-06 00:08:54 -0800

    dspd - practical 1a

--- .vscode/PRACTICAL1A.c
+++ .vscode/PRACTICAL1A.c
@@ -4,32 +4,33 @@ int main()
 {
     int a[100];
     int n;
+    
     printf("Enter the number of elements: ");
     scanf("%d", &n);
     
-    int i;
-    for(i = 0; i < n; i++)
+    printf("Enter the elements:\n");
+    for(int i = 0; i < n; i++)
     {
-        printf("Enter the element %d: ", i + 1);
+        printf("Enter the element %d: ", i+1);
         scanf("%d", &a[i]);
     }
     
-    for(i = 0; i < n; i++)
-    {
-        printf("%d ", a[i]);
-    }
-    
     int sume = 0, sumo = 0;
-    for(i = 0; i < n; i++)
+    
+    for(int i = 0; i < n; i++)
     {
         if(i % 2 == 0)
+        {
             sume = sume + a[i];
+        }
         else
+        {
             sumo = sumo + a[i];
+        }
     }
     
-    printf("\nThe sum of the elements at even index is %d", sume);
-    printf("\nThe sum of the elements at odd index position is %d", sumo);
+    printf("The sum of the elements at even index is %d\n", sume);
+    printf("The sum of the elements at odd index is %d\n", sumo);
     
     return 0;
 }
